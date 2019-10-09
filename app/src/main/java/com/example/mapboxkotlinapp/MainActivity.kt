a487daa73535729d4b27f5b5c14af349a091b475
vinhnguyen <dacvinhng@gmail.com>
2019-10-09 17:47:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mapboxkotlinapp/MainActivity.kt
@@ -0,0 +1,243 @@
+package com.example.mapboxkotlinapp
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener  {
+
+    private lateinit var mapView: MapView
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+    private lateinit var map: MapboxMap
+    private var currentRoute: DirectionsRoute? = null
+    private val TAG = "DirectionsActivity"
+    private var navigationMapRoute: NavigationMapRoute? = null
+    private lateinit var startButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.access_token))
+        setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.map = mapboxMap
+        mapboxMap.setStyle(Style.TRAFFIC_DAY){
+            enableLocationComponent(it)
+            addDestinationIconSymbolLayer(it)
+            map.addOnMapClickListener(this@MainActivity)
+            startButton = findViewById(R.id.startButton)
+            startButton.setOnClickListener(View.OnClickListener {
+                val simulateRoute = true
+                val options = NavigationLauncherOptions.builder()
+                    .directionsRoute(currentRoute)
+                    .shouldSimulateRoute(simulateRoute)
+                    .build()
+                // Call this method with Context from within an Activity
+                NavigationLauncher.startNavigation(this@MainActivity, options)
+            })
+        }
+    }
+
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            // Create and customize the LocationComponent's options
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.mapboxWhite))
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .locationComponentOptions(customLocationComponentOptions)
+                .build()
+
+            // Get an instance of the LocationComponent and then adjust its settings
+            map.locationComponent.apply {
+
+                // Activate the LocationComponent with options
+                activateLocationComponent(locationComponentActivationOptions)
+
+                // Enable to make the LocationComponent visible
+                isLocationComponentEnabled = true
+
+                // Set the LocationComponent's camera mode
+                cameraMode = CameraMode.TRACKING
+
+                // Set the LocationComponent's render mode
+                renderMode = RenderMode.COMPASS
+            }
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocationComponent(map.style!!)
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    )
+    {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapClick(point: LatLng): Boolean {
+        val destinationPoint = Point.fromLngLat(point.longitude, point.latitude)
+        val originPoint = Point.fromLngLat(
+            map.locationComponent.getLastKnownLocation()!!.longitude,
+            map.locationComponent.getLastKnownLocation()!!.latitude
+        )
+
+        val source = map.style!!.getSourceAs<GeoJsonSource>("destination-source-id")
+        source?.setGeoJson(Feature.fromGeometry(destinationPoint))
+        getRoute(originPoint, destinationPoint)
+        startButton.setEnabled(true)
+        startButton.setBackgroundResource(R.color.mapboxBlue)
+        return true;
+    }
+
+    private fun addDestinationIconSymbolLayer(loadedMapStyle: Style) {
+        loadedMapStyle.addImage(
+            "destination-icon-id",
+            BitmapFactory.decodeResource(this.resources, R.drawable.mapbox_marker_icon_default)
+        )
+        val geoJsonSource = GeoJsonSource("destination-source-id")
+        loadedMapStyle.addSource(geoJsonSource)
+        val destinationSymbolLayer =
+            SymbolLayer("destination-symbol-layer-id", "destination-source-id")
+        destinationSymbolLayer.withProperties(
+            iconImage("destination-icon-id"),
+            iconAllowOverlap(true),
+            iconIgnorePlacement(true)
+        )
+        loadedMapStyle.addLayer(destinationSymbolLayer)
+    }
+
+    private fun getRoute(origin: Point, destination: Point) {
+        NavigationRoute.builder(this)
+            .accessToken(Mapbox.getAccessToken()!!)
+            .origin(origin)
+            .destination(destination)
+            .build()
+            .getRoute(object : Callback<DirectionsResponse> {
+                override fun onResponse(
+                    call: Call<DirectionsResponse>,
+                    response: Response<DirectionsResponse>
+                ) {
+                    // You can get the generic HTTP info about the response
+                    Log.d(TAG, "Response code: " + response.code())
+                    if (response.body() == null) {
+                        Log.e(
+                            TAG,
+                            "No routes found, make sure you set the right user and access token."
+                        )
+                        return
+                    } else if (response.body()!!.routes().size < 1) {
+                        Log.e(TAG, "No routes found")
+                        return
+                    }
+
+                    currentRoute = response.body()!!.routes()[0]
+                    // Draw the route on the map
+                    if (navigationMapRoute != null) {
+                        navigationMapRoute?.removeRoute()
+                    } else {
+                        navigationMapRoute =
+                            NavigationMapRoute(null, mapView, map, R.style.NavigationMapRoute)
+                    }
+                    navigationMapRoute?.addRoute(currentRoute)
+                }
+
+                override fun onFailure(call: Call<DirectionsResponse>, throwable: Throwable) {
+                    Log.e(TAG, "Error: " + throwable.message)
+                }
+            })
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+}
